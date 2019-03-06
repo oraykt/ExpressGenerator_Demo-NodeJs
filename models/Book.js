3edf629ebda80bd72e3111f8b41783d0ad8d4fce
OrayKT <oray.kt@gmail.com>
2019-03-06 14:48:21 +0100

    mongoDb operations, searchOne searchById searchAll update and updateMulti

--- models/Book.js
+++ models/Book.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
+/*
+* type
+* default
+* required
+* unique
+*/
 const BookSchema = new Schema({
-    title: String
+    title: {
+        type: String,
+        required: true
+    },
+    published: {
+        type: Boolean,
+        default: true
+    },
+    comments: [
+        {
+            message: String
+        }
+    ],
+    meta: {
+        votes: Number,
+        favs: Number
+    }
 });
 
 module.exports = mongoose.model('book', BookSchema);
--- routes/books.js
+++ routes/books.js
@@ -3,9 +3,26 @@ const router = express.Router();
 // Models
 const Book = require('../models/Book');
 
-router.get('/new', function (req, res, next) {
+router.post('/new', function (req, res, next) {
     const book = new Book({
-        title: 'The Cleaner'
+        title: 'Course',
+
+        // published: false,
+        // comments: [
+        //     {
+        //         message: "Aweful!"
+        //     },
+        //     {
+        //         message: "I'd ever seen like this book before."
+        //     },
+        //     {
+        //         message: "I don't like"
+        //     }
+        // ],
+        // meta: {
+        //     votes: 34,
+        //     favs: 13
+        // }
     });
     book.save((err, data) => {
         if (!err) {
@@ -14,6 +31,50 @@ router.get('/new', function (req, res, next) {
             console.log(err);
         }
     })
+});
+
+router.get('/search', (req, res) => {
+    Book.find({ title: 'Course' }, (err, data) => {
+        if (!err) {
+            res.json(data);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+router.get('/searchOne', (req, res) => {
+    Book.findOne({ title: 'Course' }, (err, data) => {
+        if (!err) {
+            res.json(data);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+router.get('/searchById', (req, res) => {
+    Book.findById('5c7fcdac8680a54a1015fa2e', (err, data) => {
+        if (!err) {
+            res.json(data);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+router.put('/update', (req, res) => {
+    Book.update(
+        { published: true },
+        { published: false },
+        { multi: true },
+        (err, data) => {
+            if (!err) {
+                res.json(data);
+            } else {
+                console.log(err);
+            }
+        })
 });
 
 module.exports = router;
